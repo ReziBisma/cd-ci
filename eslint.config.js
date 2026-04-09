60972518114694d3d2308def6708e458b9b7edbd
ReziBisma <bismarezi@gmail.com>
2026-04-09 23:29:13 +0700

    fix hopefully this is the last

--- eslint.config.js
+++ eslint.config.js
@@ -4,10 +4,17 @@ module.exports = [
     languageOptions: {
       ecmaVersion: 2021,
       sourceType: "commonjs",
+      globals: {
+        console: "readonly",
+        module: "readonly",
+        require: "readonly",
+        test: "readonly",
+        expect: "readonly"
+      }
     },
     rules: {
       "no-unused-vars": "warn",
-      "no-undef": "error",
-    },
-  },
+      "no-undef": "error"
+    }
+  }
 ];
